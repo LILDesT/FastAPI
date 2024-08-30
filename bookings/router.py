997d5f4983514d5771a0bfe7d0542403c15d848b
Shamil Mazitov <shamiltron@yandex.com>
2024-08-30 15:30:33 +0300

    async def

--- bookings/router.py
+++ bookings/router.py
@@ -1,14 +1,22 @@
+from sqlalchemy import select
 from fastapi import APIRouter
+from FastAPI.database import async_session_maker
+from FastAPI.bookings.models import Bookings
+from FastAPI.bookings.dao import BookingDAO
 
 router = APIRouter(
     prefix="/bookings",
     tags=["Бронирование"],
     
 )
+@router.get("/bookings")
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Bookings)  # SELECT * FROM bookings;
+        result = await session.execute(query)
+        bookings = result.mappings().all()  # Extract the results
+        return bookings
+    
 @router.get("")
-def get_bookings():
-    pass
-
-@router.get("/{booking_id}")
-def get_bookings2(booking_id):
-    pass
+async def get_bookings2():
+    return await BookingDAO.find_all()
